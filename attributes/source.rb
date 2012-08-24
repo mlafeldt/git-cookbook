00b7350675e0b1dce68534e9b75b1df24761cfb1
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2012-08-24 15:15:21 +0200

    Use node.git.version

--- attributes/source.rb
+++ attributes/source.rb
@@ -1,2 +1,2 @@
 default[:git][:version] = "master"  # e.g. "v1.7.12-rc3"
-default[:git][:url] = "https://github.com/gitster/git/tarball/#{node[:git][:version]}"
+default[:git][:url] = "https://github.com/gitster/git/tarball/#{node.git.version}"
