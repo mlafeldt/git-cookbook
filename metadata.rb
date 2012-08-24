4a5f0b62a9de5e168fc4a184b86042b399395605
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2012-08-24 17:17:58 +0200

    Add metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+maintainer        "Mathias Lafeldt"
+maintainer_email  "mathias.lafeldt@gmail.com"
+license           "Apache 2.0"
+description       "Installs Git"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "1.0.0"
+recipe            "git::source", "Installs Git from source"
+
+supports          "ubuntu"
+supports          "debian"
