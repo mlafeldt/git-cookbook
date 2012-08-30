efb0355b0b1a3facb2062c9fcbcf0641b2554971
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2012-08-30 10:50:34 +0200

    Install git version 1.7.12

--- attributes/source.rb
+++ attributes/source.rb
@@ -1,8 +1,8 @@
-git_version = "1.7.12-rc3"
+git_version = "1.7.12"
 
 default[:git] = {
   :version   => git_version,
   :url       => "https://github.com/gitster/git/tarball/v#{git_version}",
-  :checksum  => "1ea2d52f20f2",
+  :checksum  => "382a4627ea79",
   :prefix    => "/usr/local"
 }
