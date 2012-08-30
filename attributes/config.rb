decb68064480100781b54a66db3f1aacf273cd9b
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2012-08-30 15:11:35 +0200

    KISS

--- attributes/config.rb
+++ attributes/config.rb
@@ -1,6 +1,7 @@
-default[:git][:config] = {
-  :color => {:ui => 'true'},
-  :push  => {:default => 'simple'},
-  :diff  => {:renames => 'copies'},
-  :alias => {:ci => 'commit -v', :di => 'diff', :st => 'status -sb'}
-}
+default[:git][:config][:color][:ui] = "true"
+default[:git][:config][:push][:default] = "simple"
+default[:git][:config][:diff][:renames] = "copies"
+# aliases
+default[:git][:config][:alias][:ci] = "commit -v"
+default[:git][:config][:alias][:di] = "diff"
+default[:git][:config][:alias][:st] = "status -sb"
--- attributes/source.rb
+++ attributes/source.rb
@@ -1,8 +1,4 @@
-git_version = "1.7.12"
-
-default[:git] = {
-  :version   => git_version,
-  :url       => "https://github.com/gitster/git/tarball/v#{git_version}",
-  :checksum  => "382a4627ea79",
-  :prefix    => "/usr/local"
-}
+default[:git][:version] = "1.7.12"
+default[:git][:url] = "https://github.com/gitster/git/tarball/v#{node.git.version}"
+default[:git][:checksum] = "382a4627ea79"
+default[:git][:prefix] = "/usr/local"
