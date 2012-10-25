4319bb35d1194f5bd4d13645d42def8c5a5b8193
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2012-10-25 12:25:46 +0200

    Bump cookbook version to 1.0.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "mathias.lafeldt@gmail.com"
 license           "Apache 2.0"
 description       "Installs Git from source and optionally configures it"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.0"
+version           "1.0.1"
 recipe            "git::source", "Installs Git from source"
 recipe            "git::config", "Provisions a system-wide Git config"
 
