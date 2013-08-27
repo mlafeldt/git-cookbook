33e1ea831d42c3096406c00c5b7e7ceeb2335428
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-08-27 17:43:15 +0200

    Use chef-bones gem

--- Gemfile
+++ Gemfile
@@ -1,7 +1,3 @@
 source 'https://rubygems.org/'
 
-gem 'berkshelf',  '~> 1.4.0'
-gem 'chef',       '~> 10.0'  # ChefSpec isn't Chef 11 ready yet
-gem 'chefspec'
-gem 'foodcritic', '~> 2.0.1'
-gem 'rake'
+gem 'chef-bones'
--- spec/unit/source_spec.rb
+++ spec/unit/source_spec.rb
@@ -1,7 +1,17 @@
-require 'spec_helper'
+require 'chef_bones/unit_spec_helper'
 
 describe 'The recipe git::source' do
-  let (:chef_run) { ChefSpec::ChefRunner.new.converge 'git::source' }
+  let (:chef_run) do
+    chef_run = ChefSpec::ChefRunner.new(
+      :platform      => 'ubuntu',
+      :version       => '12.04',
+      :log_level     => :error,
+      :cookbook_path => COOKBOOK_PATH
+    )
+    Chef::Config.force_logger true
+    chef_run.converge 'git::source'
+    chef_run
+  end
 
   it 'should include the recipe build-essential' do
     chef_run.should include_recipe 'build-essential'
