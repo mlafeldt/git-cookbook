af7ef11888f0053f89781586f492e25b4978b36e
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-09-08 22:52:29 +0200

    Use let in integration test

--- spec/integration/default/config_spec.rb
+++ spec/integration/default/config_spec.rb
@@ -1,6 +1,6 @@
 require 'chef_bones/integration_spec_helper'
 
-describe 'The git::config recipe' do
+describe 'The recipe git::config' do
   it 'creates the system-wide git config file' do
     expect(file '/usr/local/etc/gitconfig').to be_file
   end
--- spec/integration/default/source_spec.rb
+++ spec/integration/default/source_spec.rb
@@ -1,7 +1,7 @@
 require 'chef_bones/integration_spec_helper'
 
-describe 'The git::source recipe' do
-  git_version = '1.8.4'
+describe 'The recipe git::source' do
+  let(:git_version) { '1.8.4' }
 
   it "installs git #{git_version}" do
     expect(command 'git --version').to return_stdout /^git version #{git_version}/
