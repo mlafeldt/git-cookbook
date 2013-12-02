3d2cdfde8ac3b69c0de10fd9abd32e6b1639ad1d
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-12-02 21:32:25 +0100

    Tweak integration tests

--- test/integration/default/serverspec/config_spec.rb
+++ test/integration/default/serverspec/config_spec.rb
@@ -1,7 +1,7 @@
-require 'spec_helper'
+require "spec_helper"
 
-describe 'The recipe git::config' do
-  it 'creates the system-wide git config file' do
-    expect(file '/usr/local/etc/gitconfig').to be_file
+describe "git::config" do
+  it "creates the system-wide git config file" do
+    expect(file "/usr/local/etc/gitconfig").to be_file
   end
 end
--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -1,4 +1,4 @@
-require 'serverspec'
+require "serverspec"
 
 include Serverspec::Helper::Exec
 include Serverspec::Helper::DetectOS
@@ -6,6 +6,6 @@ include Serverspec::Helper::DetectOS
 RSpec.configure do |c|
   c.before :all do
     c.os = backend(Serverspec::Commands::Base).check_os
-    c.path = '/sbin:/usr/sbin'
+    c.path = "/sbin:/usr/sbin"
   end
 end
