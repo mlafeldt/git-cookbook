87b6e89e6c4d9aed548efd128795948be469b0f5
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-12-03 22:35:52 +0100

    Remove superfluous line from serverspec spec helper

--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -5,7 +5,6 @@ include Serverspec::Helper::DetectOS
 
 RSpec.configure do |c|
   c.before :all do
-    c.os = backend(Serverspec::Commands::Base).check_os
     c.path = "/sbin:/usr/sbin"
   end
 end
